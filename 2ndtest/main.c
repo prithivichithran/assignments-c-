0dd0d24dc905b991423c5fa8d58d3882523296fb
prithivichithran <prithivichithran@gmail.com>
2018-06-04 21:05:17 +0530

    Changed datatype to double

--- 2ndtest/main.c
+++ 2ndtest/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int l,b,Area; //Initializing the Required Data types for Specific Pgm
+    double l,b,Area,perimeter; //Initializing the Required Data types for Specific Pgm
     printf("Enter The Value Of Length\n"); //Getting The Length From User
-    scanf("%d",&l);
+    scanf("%lf",&l);
     printf("Enter The Value Of Breadth\n"); //Getting The Breadth From User
-    scanf("%d",&b);
+    scanf("%lf",&b);
+    perimeter=2*(l+b);
     Area=l*b;
-    printf("The Area Of Rectangle is %d\n",Area);
+    printf("The Area Of Rectangle is %lf\n",Area);
+    printf("Perimeter Of Rectangle is %lf\n",perimeter);
     return 0;
 }
