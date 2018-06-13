57add2e3eba6631ea1b5e2959b6ba00a431c96ab
prithivichithran <prithivichithran@gmail.com>
2018-06-13 19:29:50 +0530

    Changed Code for optimization

--- min2yr2days.c
+++ min2yr2days.c
@@ -2,13 +2,19 @@
 int main()
 {
     int min;
-    double hr,day,month,year;
+    double day,year,month,week,hr,y,d,m,h,w;
     printf("Enter Minutes\n");
     scanf("%d",&min);
-    hr=min/60;
-    day=hr/12;
-    month=day/28;
-    year=month/12;
-    printf("Year is %f\nDays is %f\nHours are %f\nMonths are %f\n",year,day,hr,month);
+    y=60*24*365;
+    d=60*24;
+    m=60*24*28;
+    w=60*24*7;
+    h=60;
+    year=(min/y);
+    day=(min/d);
+    hr=(min/h);
+    month=(min/m);
+    week=(min/w);   
+    printf("Year is %f\nDays is %f\nHours are %f\nMonths are %f\nWeeks are %f\n",year,day,hr,month,week);
     return (0);
 }
