4697658cffc960581e2c23d5a8fbeefa20175d80
prithivichithran <prithivichithran@gmail.com>
2018-06-13 20:13:47 +0530

    Basic Size operator

---
+++ basicsizeof.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%d is the size of int\n",sizeof(int));
+	printf("%d is the size of float\n",sizeof(float));
+	printf("%d is the size of char\n",sizeof(char));
+	printf("%d is the size of short\n",sizeof(short));
+	printf("%d is the size of double\n",sizeof(double));
+	printf("%d is the size of long\n",sizeof(long));
+	printf("%d is the size of longlong\n",sizeof(long long));
+	printf("%d is the size of double long\n",sizeof(double long));	
+	return 0;
+}
+
