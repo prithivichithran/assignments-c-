deb7bf8dfa50f31cd90805c7df1495969bbcffe7
prithivichithran <prithivichithran@gmail.com>
2018-06-19 23:05:30 +0530

    added extended asci to print box

---
+++ boxprint.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    {
+    printf("\u2554\u2550\u2566\u2550\u2557\n");
+    printf("\u2560\u2550\u256C\u2550\u2563\n");
+    printf("\u255A\u2550\u2569\u2550\u255D\n");
+    }
+}
+
