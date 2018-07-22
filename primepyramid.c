1d7d98a2d818d522baedb6195de5abe5297f1777
prithivichithran <prithivichithran@gmail.com>
2018-07-22 12:38:34 +0530

    Added Printing Prime Numbers in Star Pattern

---
+++ primepyramid.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int prime(int num) 
+{
+int a,flag;
+for(a=2;a<num;a++) 
+{
+if(num%a!=0)
+{
+flag=1;
+}
+else 
+{
+flag=0;
+break;
+}
+}
+if(flag==1||num==2)
+return(1);
+else
+return(0);
+}
+int main()
+{
+int i,j,k,rows,num;
+printf("Enter number of rows: ");
+scanf("%d",&rows);
+for(i=1; i<=rows;i++)
+{
+for(j=1;j<=rows-i;j++)
+{
+printf("  ");
+}
+for(k=0;k!=2*i-1;k++)
+{
+while(!prime(num)) 
+{
+num++;
+}
+printf("%d ",num++); 
+}
+printf("\n");
+}
+return 0;
+}
